1641f9c8b6e417d1ce54d9988eaacb961fd30038
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-12-01 21:42:03 +0100

    adding orders component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,32 +87,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // this.setState({ loading: true })
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Nenad Dimitrijevic',
-        //         address: {
-        //             street: 'Jorgandzijska 1',
-        //             zipCode: '11000',
-        //             country: 'Serbia'
-        //         },
-        //         email: 'test@test.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(() => {
-        //         this.setState({ loading: false, purchasing: false })
-        //     })
-        //     .catch(() => {
-        //         this.setState({ loading: false, purchasing: false })
-        //     })
+    
         const queryParams = [];
         for(let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&')
         this.props.history.push({
             pathname: '/checkout',
