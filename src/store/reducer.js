4a71ee75bb2cbaec8239f2b4df2f7995d82b6ae8
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-12-07 23:21:14 +0100

    creating store

---
+++ src/store/reducer.js
@@ -0,0 +1,29 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4
+}
+
+const reducer = (state=initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
+            }
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
+            }
+    }
+}
+
+export default reducer;
